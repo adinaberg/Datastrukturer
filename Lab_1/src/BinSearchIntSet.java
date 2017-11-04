7d3d2c9da842cd5e2298ba82541d352c2d673033
Adina Berg <adinab@ft4011-20.ft4011.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-04 16:00:04 +0100

    ska skriva remove

--- Lab_1/src/BinSearchIntSet.java
+++ Lab_1/src/BinSearchIntSet.java
@@ -1,116 +1,190 @@
 import java.util.Arrays;
 
 public class BinSearchIntSet implements IntSet {
-	
+
 	int[] set;
-	int size = 4;
-	
+	int size;
+
 	public BinSearchIntSet () {
 		set = new int[5];	
 		set[0] = -1;
-		set[1] = -4;
+		set[1] = 2;
 		set[2] = 3;
 		set[3] = 5;
 		set[4] = 10;
+		size = 5;
 	}
-	
-	//pillipill
-    public void add(int element) {
-    	
-    	
-    	int min = 0;
-    	int max = size - 1;
-    	
-    	while (max - min > 0) {
-    		System.out.println("min: " + min + " max: " + max);
-    		int middle = (max + min)/2;
-    		int candidate = set[middle];
-    		if (candidate == element) {
-    			//element already in set, break while
-    			System.out.println("Element already in set");
-    			break;
-    		}
-    		//element is in higher half
-    		else if (candidate < element) {
-        		System.out.println("Updating min");
-    			min = middle + 1;
-    		}
-    		//element is in lower half
-    		else {
-        		System.out.println("Updating max");
-    			max = middle;
-    		}   		
 
-    	}
-		System.out.println("After while  min: " + min + " max: " + max);
-    	
-    	//add element
-    	if (max - min == 0) {
-    		//if there is space left
-    		if (set.length - size > 0) {
-    			
-    			//shift elements 1 step
-    			for (int i = 0; i < size - max; i++) {
-    				set[size - i] = set[size - 1 - i];
-    			}
-    			set[max] = element;
-    		}
-    	}
-    	
-    	System.out.println(Arrays.toString(set));
-    }
-    	
-    
-    
-    public boolean contains(int element) {
-    	int min = 0;
-    	int max = size - 1;
-    	while (max - min > 0) {
-    		int middle = (max + min)/2;
-    		int candidate = set[middle];
-    		if (candidate == element) {
-    			//element found
-    			return true;
-    		}
-    		//element is in higher half
-    		else if (candidate < element) {
-    			min = middle + 1;
-    		}
-    		//element is in lower half
-    		else {
-    			max = middle;
-    		}   		
-    	}
-    	System.out.println("set to false");
-    	return false;	
-    }
-    public void remove(int element) {
-    	
-    }
-    
-    public int[] get() {
-    	return set;
-    }
-    
-	
-	
+	public void add(int element) {
+
+		int min = 0;
+		int max = size - 1;
+
+		while (max - min > 0) {
+			int middle = (max + min)/2;
+			int candidate = set[middle];
+			if (candidate == element) {
+				//element already in set, break while
+				break;
+			}
+			//element is in higher half
+			else if (candidate < element) {
+				min = middle + 1;
+			}
+			//element is in lower half
+			else {
+				max = middle;
+			}   		
+
+		}
+
+		//catch special case
+		if (max == size - 1) {
+			//check last element in set
+			if(set[max] == element) {
+				//don't enter next if
+				max++;
+			}
+			//add element larger than current largest
+			else if (set[max] < element) {
+				max++;
+				min++;
+			}
+		}
+
+		//found index of new array and last index of set does not contain element
+		if (max - min == 0) {
+			//if there is space left
+			if (set.length - size > 0) {
+
+				//shift elements 1 step
+				for (int i = 0; i < size - max; i++) {
+					set[size - i] = set[size - 1 - i];
+				}
+				set[max] = element;
+				//update size of set
+			}
+
+			//increase set capacity
+			else {
+				int[] newSet = new int[2*set.length];
+				//copy from old set (below new element)
+				for (int i = 0; i < max; i++) {
+					newSet[i] = set[i];
+				}
+				//add new element
+				newSet[max] = element;
+				//copy from old set (above new element)
+				for (int i = 0; i < size - max; i++) {
+					newSet[max + 1 + i] = set[max + i];
+				}
+				set = newSet;
+			}
+			//update size of set
+			size++;
+
+		}
+
+	}
+
+
+	public void remove(int element) {
+		int min = 0;
+		int max = size - 1;
+		int middle;
+		
+		while (max - min > 0) {
+			middle = (max + min)/2;
+			int candidate = set[middle];
+			if (candidate == element) {
+				//element already in set, break while
+				break;
+			}
+			//element is in higher half
+			else if (candidate < element) {
+				min = middle + 1;
+			}
+			//element is in lower half
+			else {
+				max = middle;
+			}   		
+
+		}
+		
+		
+		if (!(max - min == 0)) {
+			if (!(set[max] == element)) {
+				
+			}
+		}
+		
+		
+	}
+
+
+
+
+
+	public boolean contains(int element) {
+		int min = 0;
+		int max = size - 1;
+		while (max - min > 0) {
+			int middle = (max + min)/2;
+
+			int candidate = set[middle];
+			if (candidate == element) {
+				//element found
+				return true;
+			}
+			//element is in higher half
+			else if (candidate < element) {
+				min = middle + 1;
+			}
+			//element is in lower half
+			else {
+				max = middle - 1;
+			}		
+		}
+		//if element is last
+		if (set[max] == element) {
+			return true;
+
+		}
+		return false;	
+	}
+
+
+	public int[] getSet() {
+		return set;
+	}
+	public int getSize() {
+		return size;
+	}
+
+
 	public static void main(String args[]) {
-		
-		
+
+
 		BinSearchIntSet test = new BinSearchIntSet();
-		System.out.println(Arrays.toString(test.get()));
+		System.out.println("Set: " + Arrays.toString(test.getSet()) + " Size: " + test.getSize());
 
 
-		
-		boolean t = test.contains(-2);
+		//boolean t = test.contains(10);
+		//System.out.println(t);
+
+		test.add(1);
+		System.out.println("Set: " + Arrays.toString(test.getSet()) + " Size: " + test.getSize());
+
+		test.add(4);
+		System.out.println("Set: " + Arrays.toString(test.getSet()) + " Size: " + test.getSize());
+
+		test.add(11);
+		System.out.println("Set: " + Arrays.toString(test.getSet()) + " Size: " + test.getSize());
+
+
+		boolean t = test.contains(-1);
 		System.out.println(t);
-
-		System.out.println(t);
-
-		//test.add(3);
-	
-		
 
 	}
-	
+
 }
