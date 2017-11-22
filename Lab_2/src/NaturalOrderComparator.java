ca18d71ceb3bf3de16c03e7fdc4a1138d957ea81
Prem0nition <jonathanhansson@ymail.com>
2017-11-22 15:50:06 +0100

    Comparator works

--- Lab_2/src/NaturalOrderComparator.java
+++ Lab_2/src/NaturalOrderComparator.java
@@ -7,4 +7,6 @@ public class NaturalOrderComparator<T extends Comparable<? super T>> implements 
     public int compare(T o1, T o2) {
         return o1.compareTo(o2);
     }
+    
+    
 }
--- Lab_2/src/StockTrade.java
+++ Lab_2/src/StockTrade.java
@@ -1,13 +1,24 @@
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class StockTrade {
-    private PrioQueue<Bid> sellersQueue;
+    private BinHeap<Bid> sellersQueue;
     private PrioQueue<Bid> buyersQueue;
 
-    public StockTrade() { ... }
+    public StockTrade() {  
+    	Comparator<Bid> comp = new BidComparator();
+    	sellersQueue = new BinHeap<Bid>(comp);
+    	Bid bid1 = new Bid("Bosse",53);
+    	sellersQueue.add(bid1);
+    	System.out.println(sellersQueue.getList());
+    	
+    	
+    }
 
-    public Transaction placeSellBid(Bid bid) { ... }
-    public Transaction placeBuyBid(Bid bid) { ... }
+    
+    
+    public Transaction placeSellBid(Bid bid) { return null;    }
+    public Transaction placeBuyBid(Bid bid) { return null; }
 
     public Iterator<Bid> sellBidsIterator() {
         return sellersQueue.iterator();
@@ -16,4 +27,24 @@ public class StockTrade {
     public Iterator<Bid> buyBidsIterator() {
         return buyersQueue.iterator();
     }
+    public static void main(String[] args){
+    	StockTrade test = new StockTrade();
+    }
+    
+
+    //from java 8 there is static method Comparator.naturalOrder(), but for java 7 this is needed
+    private class BidComparator implements Comparator<Bid> {
+    	@Override
+    	public int compare(Bid o1, Bid o2) {
+    		Integer p1 = o1.price;
+    		Integer p2 = o2.price;
+    		return p1.compareTo(p2);
+    	}
+
+
+
+    }
 }
+
+
+
