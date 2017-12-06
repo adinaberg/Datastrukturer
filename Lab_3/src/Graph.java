1433fd53bb7561d284db32f9ab33f49163050f08
Prem0nition <jonathanhansson@ymail.com>
2017-12-06 10:12:58 +0100

    Vi har i alla fall addV och addE nu

--- Lab_3/src/Graph.java
+++ Lab_3/src/Graph.java
@@ -12,11 +12,22 @@ public class Graph {
 
 	public void addVertice(String label) {
 		
-		
+		map.put(label,new ArrayList<>());
+		System.out.println(map);
 	}
 
 	public void addEdge(String node1, String node2, int dist) {
-
+		if(!map.containsKey(node1) || !map.containsKey(node2)){
+			System.out.println("Entered an invalid node!");
+		}else{
+			Pair<String, Integer> p1 = new Pair(node1,dist);	// Path to node1 with distance to be added to node2
+			map.get(node2).add(p1);	// Add the path from node2 to node1
+			System.out.println(map);
+			
+			Pair<String, Integer> p2 = new Pair(node2,dist);
+			map.get(node1).add(p2);	// Add the path from node1 to node2
+			System.out.println(map);
+		}
 	}
 
 	public static class Path {
--- Lab_3/src/Main.java
+++ Lab_3/src/Main.java
@@ -4,15 +4,28 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		APrioMap<String, Integer> apr = new APrioMap<>();
+		/*APrioMap<String, Integer> apr = new APrioMap<>();
 		apr.put("First", 5);
 		apr.put("Second", 2);
 		apr.put("Thrird", 9);
 		apr.put("Fourth", 1);
+		apr.put("Fifth", 2);
 		System.out.println(apr.getPrioList());
 		System.out.println(apr.poll());
-		System.out.println(apr.get("Fourth"));
+		System.out.println(apr.get("Fourth"));*/
 		
+		Graph g = new Graph();
+		g.addVertice("A");
+		g.addVertice("B");
+		g.addVertice("C");
+		g.addVertice("D");
+		g.addVertice("E");
+		g.addVertice("F");
+		g.addEdge("A", "B", 2);
+		g.addEdge("B", "E", 3);
+		g.addEdge("A", "D", 7);
+		g.addEdge("D", "E", 1);
+		g.addEdge("B", "C", 1);
 	}
 
 }
